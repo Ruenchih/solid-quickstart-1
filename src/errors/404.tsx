534139186e2ac074ff7227ab9bb1c57acd8f646d
Ruenchih <github@tsetung.com>
2025-01-15 17:33:25 +0800

    404.tsx

--- src/errors/404.tsx
+++ src/errors/404.tsx
@@ -1,8 +1,8 @@
 export default function NotFound() {
   return (
     <section class="bg-red-100 text-slate-700 p-8 rounded-md">
-      <h2 class="text-2xl">404: Not Found</h2>
-      <p class="mt-4">It's gone 😞!</p>
+      <h2 class="text-2xl">萧瑟秋风今又是，换了人间</h2>
+      <p class="mt-4">中华儿女多奇志，不爱红装爱武装</p>
     </section>
   );
 }
